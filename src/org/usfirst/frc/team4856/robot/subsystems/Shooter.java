343aa5c4bd540933c913234f64df2913400765b1
bcdx <pakavoor@chapin.edu>
2016-02-16 10:36:17 -0500

    Added potentiometer code into Shooter subsystem

--- src/org/usfirst/frc/team4856/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team4856/robot/subsystems/Shooter.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4856.robot.subsystems;
 
+
 //import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.command.Subsystem;
 //import edu.wpi.first.wpilibj.Joystick;
@@ -8,23 +9,25 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.Talon;
+
 
 import org.usfirst.frc.team4856.robot.Robot;
-//import org.usfirst.frc.team4856.robot.commands.TankDriveWithJoystick;
+import org.usfirst.frc.team4856.robot.commands.TankDriveWithJoysticks;
+
 /**
  *
  */
-public class Shooter extends Subsystem {
+public class Shooter extends PIDSubsystem {
 	private DigitalInput insideContact;
     private DigitalInput outsideContact;
   
     
-    //AnalogPotentiometer pot = new AnalogPotentiometer(0, 360, 30);
     
 
 // Subsystem is the parent class of DriveTrain.
@@ -32,13 +35,34 @@ public class Shooter extends Subsystem {
 	
 	private SpeedController shooterMotor;
 	private SpeedController angleMotor;
+	private static final double Kp = -2; //Kp = proportional (present error values), in response to diff between observed + setpoint
+    private static final double Ki = 0.0; //Ki = integral(past error values), looks at past info + determines level to achieve stability
+    private static final double Kd =0.0; //Kd = differential (future error values), looks at future outcome + adjusts rate of change to compensate
+    public static final double value = 3.5,
+            value2 = 1.75; //set values?
+    AnalogPotentiometer pot = new AnalogPotentiometer(0, 360, 30);
+    
+ // Subsystem is the parent class of DriveTrain.
+ // Though inheritance, DriveTrain inherits all the traits of the class Subsystem, and will have any new traits we assign to it.
+     
+     //private SpeedController grabberMotor;
+
+
 	
 	public Shooter () {
-		super();                                                                                                                                                                                      
+		//super();
+		super("Shooter", Kp, Ki, Kd);
+        pot = new AnalogPotentiometer(0, 90, 0); //(0,270,-135) -135 < x < 135
+        AnalogInput ai = new AnalogInput(1);
+        pot = new AnalogPotentiometer(ai, 90, 0);
+       
+        setSetpoint(value);
+        enable();
 		shooterMotor = new Victor (3);//grabberMotor runs the grabber 
 		insideContact = new DigitalInput(3);
 	    outsideContact = new DigitalInput(5);
 	    angleMotor = new Victor (4);
+	    
 	    /*pot = new AnalogPotentiometer(0, 360, 30);
 	    AnalogInput ai = new AnalogInput(1);
 	    pot = new AnalogPotentiometer(ai, 360, 30);
@@ -60,6 +84,20 @@ public class Shooter extends Subsystem {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
+    protected double returnPIDInput() {
+        // Return your input value for the PID loop
+        // e.g. a sensor, like a potentiometer:
+        // yourPot.getAverageVoltage() / kYourMaxVoltage;
+        double degrees = pot.get();
+        return degrees;
+        }
+
+        protected void usePIDOutput(double output) {
+        // Use output to drive your system, like a motor
+        // e.g. yourMotor.set(output);
+        }
+
     public void pickUp() {
     	shooterMotor.set(0.62);//picking up the ball - check whether speed should be positive or negative
     }
