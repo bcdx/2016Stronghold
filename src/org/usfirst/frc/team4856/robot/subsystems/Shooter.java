d8055f7569ef7ef61a3db0059116f2fd12aa34da
computer1 <pakavoor@chapin.edu>
2016-02-18 18:29:31 -0500

    updated shooter.java by combining code from other computer

--- src/org/usfirst/frc/team4856/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team4856/robot/subsystems/Shooter.java
@@ -23,10 +23,10 @@ import org.usfirst.frc.team4856.robot.Robot;
 /**
  *
  */
-public class Shooter extends Subsystem {
-//public class Shooter extends PIDSubsystem {
-	private DigitalInput insideContact;
-    private DigitalInput outsideContact;
+//public class Shooter extends Subsystem {
+public class Shooter extends PIDSubsystem {
+	//private DigitalInput insideContact;
+    //private DigitalInput outsideContact;
   
     
     
@@ -36,44 +36,41 @@ public class Shooter extends Subsystem {
 	
 	private SpeedController shooterMotor;
 	private SpeedController angleMotor;
-	/*
+	
 	private static final double Kp = -2; //Kp = proportional (present error values), in response to diff between observed + setpoint
     private static final double Ki = 0.0; //Ki = integral(past error values), looks at past info + determines level to achieve stability
-    private static final double Kd =0.0; //Kd = differential (future error values), looks at future outcome + adjusts rate of change to compensate
+    private static final double Kd = 0.0; //Kd = differential (future error values), looks at future outcome + adjusts rate of change to compensate
     public static final double value = 3.5,
             value2 = 1.75; //set values?
-    AnalogPotentiometer pot = new AnalogPotentiometer(0, 360, 30);
+    AnalogPotentiometer pot = new AnalogPotentiometer(0, 360, 30); //not part of of la otra computadora
     
- // Subsystem is the parent class of DriveTrain.
- // Though inheritance, DriveTrain inherits all the traits of the class Subsystem, and will have any new traits we assign to it.
-     
      //private SpeedController grabberMotor;
-*/
 
+    AnalogInput ai = new AnalogInput(1);
+    pot = new AnalogPotentiometer(ai, 90, 0);
 	
 	public Shooter () {
-		super();
-		//super("Shooter", Kp, Ki, Kd);
+		super("Shooter", Kp, Ki, Kd);
         /*
 		pot = new AnalogPotentiometer(0, 90, 0); //(0,270,-135) -135 < x < 135
-        AnalogInput ai = new AnalogInput(1);
-        pot = new AnalogPotentiometer(ai, 90, 0);
+        
        
         setSetpoint(value);
-        enable();
+
         */
 		shooterMotor = new Victor (3);//grabberMotor runs the grabber 
-		insideContact = new DigitalInput(3);
-	    outsideContact = new DigitalInput(5);
+		//insideContact = new DigitalInput(3);
+	    //outsideContact = new DigitalInput(5);
 	    angleMotor = new Victor (4);
-	    
+        enable();
+        
 	    /*pot = new AnalogPotentiometer(0, 360, 30);
 	    AnalogInput ai = new AnalogInput(1);
 	    pot = new AnalogPotentiometer(ai, 360, 30);
 	    double degrees = pot.get();*/
 	    
-        LiveWindow.addActuator("insideContact", "LimitSwitch", insideContact);
-        LiveWindow.addActuator("outsideContact", "LimitSwitch", outsideContact);
+        //LiveWindow.addActuator("insideContact", "LimitSwitch", insideContact);
+        //LiveWindow.addActuator("outsideContact", "LimitSwitch", outsideContact);
 	
 	}
     
@@ -121,8 +118,24 @@ public class Shooter extends Subsystem {
     public void setAngle (Joystick stick){
     	double speed = stick.getY();
     	angleMotor.set(speed);
-    	
-    	
+    }
+    
+    public void setAngle (double angle){
+    	setSetpoint(angle);
+    }
+    
+    protected double returnPIDInput() {
+    	// return your input value for the PID loop
+    	// e.g. a sensor, like a potentiometer
+    	// yourPot.getAverageVoltage() /kYourMaxVoltage;
+    	double degrees = pot.get();
+    	return degrees;
+    }
+    
+    protected void usePIDOutput(double output){
+    	// use output to drive your system, like a motor
+    	// e.g. yourMotor.set(output);
+    	angleMotor.set(output);
     }
     
     /*public void setAngleManually(double speed) {
